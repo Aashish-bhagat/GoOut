4b22ca115a8f39f01a8949dfb3ad85c279d8392b
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2016-10-31 12:20:59 +0530

    VKD-added scale funct.

--- app/src/main/java/com/fortefort/goout/FilterActivity.java
+++ app/src/main/java/com/fortefort/goout/FilterActivity.java
@@ -1,7 +1,9 @@
 package com.fortefort.goout;
 
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class FilterActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class FilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+        final FrameLayout frame = (FrameLayout) findViewById(R.id.framef);
+        final int height = frame.getHeight();
+        final int width = frame.getWidth();
+        final float finalwidth = width/10f ;
+        final float finalheight = height/10f ;
+        ValueAnimator animator = ValueAnimator.ofFloat(0,1);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+              float namef = (float) animation.getAnimatedFraction();
+                frame.setScaleX(width - (width-finalwidth)*namef);
+                frame.setScaleY(height - (height - finalheight)*namef);
+                //frame.setTranslationY();
+            }
+        });
     }
+
 }
